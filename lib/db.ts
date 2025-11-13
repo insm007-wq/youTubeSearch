d909f92bf120d7d75790769ac1c14fb3fd1fb3de
Paul <insm007@gmail.com>
2025-11-13 13:33:54 +0900

    Revert "MongoDB 연결 최적화: 런타임 환경변수 확인으로 변경"
    This reverts commit 9873f2a23e046ab116d9af1d5205dbe441e0acc0.

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,13 @@
 import { MongoClient } from 'mongodb'
 
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please define MONGODB_URI in .env.local')
+}
+
 const uri = process.env.MONGODB_URI
 let client: MongoClient | null = null
 
 const clientPromise = (async () => {
-  if (!uri) {
-    throw new Error('Please define MONGODB_URI environment variable')
-  }
-
   if (!client) {
     client = new MongoClient(uri, {
       retryWrites: true,
